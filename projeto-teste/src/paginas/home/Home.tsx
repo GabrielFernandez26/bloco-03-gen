1e6a5e166d5f6a34e4b0944b432c28ba8e7037b9
gabriel <gabriel.f.espanhol@hotmail.com>
2024-02-14 12:47:36 -0300

    projeto teste de React

---
+++ projeto-teste/src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import './Home.css'
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo">Manipulando o mundo</h1>
+        <img 
+        className="img"
+        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43263329-3b8f-4263-b34e-6ccd4d6048f7/dfyszbb-ba5fc148-6833-4107-887c-4d97fe9459ef.jpg/v1/fit/w_828,h_1036,q_70,strp/cats_rule_the_world_by_poruarts_dfyszbb-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA1MCIsInBhdGgiOiJcL2ZcLzQzMjYzMzI5LTNiOGYtNDI2My1iMzRlLTZjY2Q0ZDYwNDhmN1wvZGZ5c3piYi1iYTVmYzE0OC02ODMzLTQxMDctODg3Yy00ZDk3ZmU5NDU5ZWYuanBnIiwid2lkdGgiOiI8PTg0MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.QyFczusQm-U9jKm49kg_ZO8P7IIrKCz86hCCivtnHC0" 
+        alt="Gato dominando o mundo" />
+        </>
+    );
+}
+
+export default Home;
